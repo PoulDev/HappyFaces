6259c58286229ff279d93ef1d097d0d67c427fd5
PoulDev <filippotrabattoni@gmail.com>
2025-05-02 08:45:31 +0200

    Dimensions fix

--- avatars.py
+++ avatars.py
@@ -2,7 +2,7 @@ import random
 import math
 
 template = '''
-<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+<svg width="80" height="80" xmlns="http://www.w3.org/2000/svg">
     {circle}
     {face}
 </svg>
