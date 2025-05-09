6fae18758f76c7b793e3a1340dd188707df67434
Wasosky <110636486+0xWasosky@users.noreply.github.com>
2025-05-09 22:06:07 +0200

    Update avatarServer.py

--- avatarServer.py
+++ avatarServer.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from avatars import randAvatar, genAvatar
+from avatars import randAvatar
 
-app = Flask(__name__)
+
+app: Flask = Flask(__name__)
+
 
 @app.route('/')
-def index():
+def index() -> str:
     return randAvatar()
 
-if __name__ == '__main__':
-    app.run('127.0.0.1', 8000)
+if __name__ == "__main__":
+    app.run("127.0.0.1", 8080)
+
